e6bc19113f3f122e4cefa85aa3a827c8f5b84ce1
Ahmed El-Demasy <39886429+demasy@users.noreply.github.com>
2022-03-17 20:53:49 +0300

    add install.sh to install npm project

---
+++ install.sh
@@ -0,0 +1,7 @@
+npm config set init-author-name "Ahmed El-Demasy" -g
+
+npm config set init-author-email "ceo@egyptianprogrammers.com" -g
+
+npm config set init-author-url "https://github.com/demasy" -g
+
+npm init -y
