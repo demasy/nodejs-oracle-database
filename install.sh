4ba946e2add5aa48651e005066239cd95a947a99
Ahmed El-Demasy <ahmed.eldidamoni@wipro.com>
2023-09-09 12:12:11 +0300

    install oracledb, sequelize, and typeorm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 npm config set init-author-name "Ahmed El-Demasy" -g
 
-npm config set init-author-email "ceo@egyptianprogrammers.com" -g
+npm config set init-author-email "founder@demasy.com" -g
 
 npm config set init-author-url "https://github.com/demasy" -g
 
