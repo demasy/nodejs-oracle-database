6c20dae41863870ad2473e2d9199d8f117f19475
Ahmed El-Demasy <39886429+demasy@users.noreply.github.com>
2025-03-12 15:23:52 +0300

    npm update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-npm config set init-author-name "Ahmed El-Demasy" -g
+npm config set init-author-name "Ahmed Demasy" -g
 
-npm config set init-author-email "founder@demasy.com" -g
+npm config set init-author-email "founder@demasy.io" -g
 
 npm config set init-author-url "https://github.com/demasy" -g
 
