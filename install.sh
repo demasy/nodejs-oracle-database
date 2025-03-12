e889d14db78ffd0cfe545a6d4baf63867c1efd2e
Ahmed El-Demasy <39886429+demasy@users.noreply.github.com>
2025-03-12 15:38:02 +0300

    Update install.sh file

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-npm config set init-author-name "Ahmed Demasy" -g
+npm config set init-author-name "Demasy" -g
 
 npm config set init-author-email "founder@demasy.io" -g
 
 npm config set init-author-url "https://github.com/demasy" -g
+
+npm config set init-license "MIT" -g
+
+npm config set init-version "1.0.0" -g
 
 npm init -y
